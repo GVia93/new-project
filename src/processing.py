d8d39a95449d0b9f90f02b2b2e6d125280a762ee
gvia <gritcaev.vv@gmail.com>
2025-01-25 15:41:28 +0300

    added func filter_by_state to  processing.py module

--- src/processing.py
+++ src/processing.py
@@ -11,3 +11,18 @@ def sort_by_date(list_info: List[Dict[str, str]], descending: bool = True) -> Li
     """
     sorted_list = sorted(list_info, key=lambda x: x['date'], reverse=descending)
     return sorted_list
+
+
+def filter_by_state(list_info: List[Dict[str, str]], state: str = 'EXECUTED') -> List[Dict[str, str]]:
+    """
+    Фильтр списка словарей по значению ключа 'state'
+
+    :param list_info: Список словарей.
+    :param state: Значение для фильтрации (по умолчанию 'EXECUTED')
+    :return: Новый список словарей с указанным значением ключа 'state'.
+    """
+    filtered_list = []
+    for item in list_info:
+        if item.get('state') == state:
+            filtered_list.append(item)
+    return filtered_list
