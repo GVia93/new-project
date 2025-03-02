07732039b0583d95d8a108d5d94c303a76afdf14
gvia <gritcaev.vv@gmail.com>
2025-03-02 22:10:34 +0300

    add test func count_transactions_by_category

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List
 
 import pytest
 
-from src.processing import filter_by_state, sort_by_date
+from src.processing import count_transactions_by_category, filter_by_state, sort_by_date
 
 
 @pytest.mark.parametrize(
@@ -79,3 +79,28 @@ def test_sort_by_date(
 def test_filter_by_state(input_list: List[Dict[str, Any]], state: str, expected_output: List[Dict[str, Any]]) -> None:
     """Тестовая функция fiter_by_state с использованием parametrize."""
     assert filter_by_state(input_list, state) == expected_output
+
+
+@pytest.fixture
+def sample_transactions():
+    return [
+        {"description": "Перевод организации"},
+        {"description": "Открытие вклада"},
+        {"description": "Перевод с карты на карту"},
+    ]
+
+
+@pytest.fixture
+def sample_categories():
+    return ["Перевод организации", "Открытие вклада", "Перевод с карты на карту"]
+
+
+def test_count_transactions_by_category(sample_transactions, sample_categories):
+    """Тестирует функцию count_transactions_by_category на корректность подсчета транзакций по категориям."""
+    expected_result = {
+        "Перевод организации": 1,
+        "Открытие вклада": 1,
+        "Перевод с карты на карту": 1,
+    }
+    result = count_transactions_by_category(sample_transactions, sample_categories)
+    assert result == expected_result
