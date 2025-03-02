230e8da2ffe145b9e1bc74fc8094bc8240bdd63e
gvia <gritcaev.vv@gmail.com>
2025-03-02 22:37:20 +0300

    fix logger

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 import logging
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler("logs/masks.log")
+file_handler = logging.FileHandler("logs/masks.log", mode="w")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
